3263711c75e6e4bcb436de4d963a250f8fb0d5a3
Vikesh09 <gvk26601@gmail.com>
2025-11-05 00:00:03 +0530

    #

--- Task.py
+++ Task.py
@@ -5,12 +5,18 @@ data ={
     "Age": [45, 42, 35, 50, np.nan, 48, 70, 30, 15, 40, 38, 36, 60, np.nan],
     "City": ["Kachh", "Ahmadabad", "Bhopal", "Bihar", "Mumbai", "Mumbai", "Kachh", "Mumbai", "Mumbai", "Mumbai", "Mumbai", "Mumbai", "Mumbai", "Mumbai"],
     "Occupation": ["Shopkeeper", "Housewife", "Journalist", "Doctor", "Job", "Teacher", "Retired", "Student","Student", "Job","Housewife","Housewife","Scientist","Shopkeeper"],
-    
+    "Education": ["High School", "High School", "Graduation", "PhD", "Graduation", "Graduation", "High School", "High School","High School", "Graduation","Graduation","Graduation","PhD","Graduation"]
 }
 df=pd.DataFrame(data)
 #ordinal Encoding
-City_mapping={"Kachh":1,"Ahmadabad":2,"Bhopal":3,"Bihar":4,"Mumbai":5}
-df["City_Encoded"] = df["City"].map(City_mapping)
+Education_mapping={"High School":1,"Graduation":2,"PhD":3}
+encoded_values=[]
+for edu in df["Education"]:
+    if edu in Education_mapping:
+        encoded_values.append(Education_mapping[edu])
+    else:
+        encoded_values.append(0)
+df["Education_Encoded"]=encoded_values
 print(df)
 
 #One Hot Encoding
@@ -22,7 +28,7 @@ for city in Cities:
             a.append(1)
         else:
             a.append(0)
-    df[f"city_{city}"]=pd.DataFrame(a)
+df[f"city_{city}"]=pd.DataFrame(a)
 print(df)
 #simple Imputer
 #mean
